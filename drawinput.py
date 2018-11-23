3d3367e6735bb93990b6f3cfecb569cd7907764b
huy.luong96@live.de <movingrubber22>
2018-11-23 18:10:24 +0100

    build "clear function" to drawinput

--- drawinput.py
+++ drawinput.py
@@ -11,7 +11,13 @@ green = (0,128,0)
 
 def save():
     filename = "image.png"
+
     image1.save(filename)
+
+def clear():
+    cv.delete("all")
+    draw.rectangle((0, 0, 200, 200), fill=(255, 255, 255, 1))
+
 
 def paint(event):
     # python_green = "#476042"
@@ -21,30 +27,30 @@ def paint(event):
     draw.line([x1, y1, x2, y2],fill="black",width=5)
 
 root = Tk()
-
 # Tkinter create a canvas to draw on
 cv = Canvas(root, width=width, height=height, bg='white')
-cv.pack()
 
+cv.pack()
 # PIL create an empty image and draw object to draw on
 # memory only, not visible
 image1 = PIL.Image.new("RGB", (width, height), white)
-draw = ImageDraw.Draw(image1)
 
 # do the Tkinter canvas drawings (visible)
 # cv.create_line([0, center, width, center], fill='green')
 
+draw = ImageDraw.Draw(image1)
 cv.pack(expand=YES, fill=BOTH)
-cv.bind("<B1-Motion>", paint)
 
 # do the PIL image/draw (in memory) drawings
 # draw.line([0, center, width, center], green)
 
+cv.bind("<B1-Motion>", paint)
 # PIL image can be saved as .png .jpg .gif or .bmp file (among others)
 # filename = "my_drawing.png"
 # image1.save(filename)
 button=Button(text="save",command=save)
 button.pack()
+button2=Button(text="clear",command=clear)
+button2.pack(side = "left")
 
 root.mainloop()
-
