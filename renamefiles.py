97e46123436bdcc985d6acd047e6effc8dc078c8
Hyu22 <Hyu22@users.noreply.github.com>
2019-05-07 17:24:50 +0200

    Delete renamefiles.py

--- renamefiles.py
+++
@@ -1,6 +0,0 @@
-import os
-#Dateiendung ändern
-counter = 0
-for filename in os.listdir('C:/Users/Hyu/Downloads/Monogramme'):
-    new_filename = filename.split('.')[0]
-    os.rename('C:/Users/Hyu/Downloads/Monogramme/' + filename, 'C:/Users/Hyu/Downloads/Monogramme/'+new_filename+'.jpg')
